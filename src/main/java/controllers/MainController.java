61b5ea22e65bfd5cfc308213c59b2126b9aa0480
stden <J9#m-2!k>
2017-01-27 22:51:32 +0300

    Подключили Spring Framework, настроили его, сделали чтобы показывалась простейшая страница из контроллера

---
+++ src/main/java/controllers/MainController.java
@@ -0,0 +1,16 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/") // Базовый путь
+public class MainController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String index(ModelMap modelMap) {
+        return "index";
+    }
+}
